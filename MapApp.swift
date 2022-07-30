9357087a6b689cd5680450ef46c0d3dc199b9fd2
yukodaira <103196220+yukodaira@users.noreply.github.com>
2022-07-30 12:40:11 +0900

    Add files via upload

---
+++ MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by Swift-Beginners on 2020/11/05.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
