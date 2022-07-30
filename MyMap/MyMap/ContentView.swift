7d92f8c98162f6982c25ef09517294a83c1cef1e
yukodaira <103196220+yukodaira@users.noreply.github.com>
2022-07-30 20:05:20 +0900

    Add files via upload

---
+++ MyMap/MyMap/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  MyMap
+//
+//  Created by 小平裕 on 2022/07/25.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // 入力中の文字列を保持する状態変数
+    @State var inputText:String = ""
+    // 検索キーワードを保持する状態変数
+    @State var dispSearchKey:String = ""
+
+    var body: some View {
+        // 垂直にレイアウト（縦方向にレイアウト）
+        VStack {
+            // テキストフィールド（文字入力）
+            TextField("キーワードを入力してください",
+                      text: $inputText , onCommit: {
+
+                // 入力が完了したので検索キーワードに設定する
+                dispSearchKey = inputText
+                
+                // 検索キーワードをデバックエリアに出力する
+                print("入力したキーワード：" + dispSearchKey)
+                
+            })
+            // 余白を追加
+            .padding()
+            
+            // マップを表示
+            MapView(searchKey: dispSearchKey)
+            
+        } // VStack ここまで
+    } // body ここまで
+} // ContentView ここまで
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMap/MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 小平裕 on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
